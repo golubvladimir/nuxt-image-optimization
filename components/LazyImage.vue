702730a2355019656314cf3a2551e059f4d873d4
Vladimir Golub <golubvlauri@gmail.com>
2022-11-24 23:11:02 +0200

    add

---
+++ components/LazyImage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="lazy-image">
+    <img
+      :src="$img(src, { quality: 10, height: 100 })"
+      v-lazy="{ src: src, loading: $img(src, { quality: 10, height: 100 }) }"
+      :alt="alt"
+    >
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "LazyImage",
+    props: {
+      src: {
+        type: String,
+        default: ''
+      },
+      alt: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .lazy-image img {
+    width: 100%;
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="page">
+    <div class="block"></div>
+    <LazyImage
+      src="/images/prague-conference-center-1056491.jpg"
+      alt="Static image"
+    />
+    <LazyImage
+      src="https://images.freeimages.com/images/large-previews/218/my-dog-cutter-1499799.jpg"
+      alt="Free image"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'IndexPage',
+    components: {
+      LazyImage: () => import('@/components/LazyImage')
+    }
+  }
+</script>
+
+<style>
+  .page {
+
+  }
+
+  .block {
+    height: 110vh;
+  }
+</style>
